548ce73b5b7d3a98f262ec5f9397d2b38dbafba1
Achyut Neupane <achyutkneupane@gmail.com>
2025-07-05 21:04:35 +0545

    feat: Job for Converting and updating database cloned

---
+++ src/Jobs/QueueHLSConversion.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AchyutN\LaravelHLS\Jobs;
+
+use AchyutN\LaravelHLS\Actions\ConvertToHLS;
+use Exception;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Storage;
+
+class QueueHLSConversion implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Indicate if the job should be marked as failed on timeout.
+     *
+     * @var bool
+     */
+    public bool $failOnTimeout = false;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     */
+    public int $timeout = 0; // No timeout, can run indefinitely
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Model $model
+    )
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     * @throws Exception
+     */
+    public function handle(): void
+    {
+        $original_path = $this->model->original_path;
+        $folderName = uuid_create();
+
+        ConvertToHLS::convertToHLS(
+            $original_path,
+            $folderName,
+            $this->model
+        );
+
+        $this->model->update([
+            'hls_path' => $folderName,
+        ]);
+
+        Storage::disk('public')->delete($original_path);
+        $this->model->update([
+            'original_path' => null,
+        ]);
+    }
+}
