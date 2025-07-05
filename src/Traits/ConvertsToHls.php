3889371d8555a3cd604a0a2e12f4acc08484fe9c
Achyut Neupane <achyutkneupane@gmail.com>
2025-07-05 21:22:50 +0545

    wip: Trait for HLS Conversion placeholder

---
+++ src/Traits/ConvertsToHls.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AchyutN\LaravelHLS\Traits;
+
+trait ConvertsToHls
+{
+    /**
+     * Boot the converts to HLS trait for a model.
+     *
+     * @return void
+     */
+    public static function bootConvertsToHls(): void
+    {
+        //
+    }
+}
